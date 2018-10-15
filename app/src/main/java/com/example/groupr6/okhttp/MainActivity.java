ca7482968f3e6f38f8946e5b8f9f9cf80e3a7a45
kshitijshah <shahksh1011@gmail.com>
2018-10-15 18:11:21 -0400

    Initial Setup

--- app/src/main/java/com/example/groupr6/okhttp/MainActivity.java
+++ app/src/main/java/com/example/groupr6/okhttp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.groupr6.okhttp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    private class AsyncTask extends android.os.AsyncTask<String ,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(strings[0]).build();
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
