bed6261eb38ef885f8f96b7e911e7a9a0bb2f694
kshitijshah <shahksh1011@gmail.com>
2018-10-15 16:29:08 -0400

    First Commit

---
+++ app/src/main/java/com/example/groupr6/okhttp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.groupr6.okhttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
